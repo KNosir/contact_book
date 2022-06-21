a9c0980de4a9f3aef84642ecc8b439bae56f9bac
KNosir <94743032+KNosir@users.noreply.github.com>
2022-06-21 13:23:01 +0500

    new changes

--- example.py
+++ example.py
@@ -1,21 +1,10 @@
 from tabulate import tabulate
-def separated_file_into_list_by_dic():
-    '''Separating text into list by dic element'''
-    f = open('data.txt', 'r')
-    read_f = f.read()
-    read_f_list = read_f.split('|')
-    free_list = []
-    for i in read_f_list:
-        if len(i) > 2:
-            i = list(map(str.strip, i.strip('][').replace('"', '').split(',')))
-            free_list.append(i)
+from main import *
+def searching(key_word):
+    for item in separated_file_into_list_by_dic():
+        result_list = []
+        if key_word in item:
+            result_list.append(item)
+    print(tabulate(result_list, floatfmt='grid'))
 
-    free_list.insert(0, ['Name', 'Phone number', 'E-mail'])
-    
-
-    return free_list
-
-
-result = separated_file_into_list_by_dic()
-
-print(tabulate(result, headers='firstrow', tablefmt='grid'))
+searching(separated_file_into_list_by_dic())
