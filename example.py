57c0092b10927f6635056e51dc4150a326095836
KNosir <94743032+KNosir@users.noreply.github.com>
2022-06-21 14:02:07 +0500

    fully completed

--- example.py
+++ example.py
@@ -1,10 +1,1 @@
-from tabulate import tabulate
-from main import *
-def searching(key_word):
-    for item in separated_file_into_list_by_dic():
-        result_list = []
-        if key_word in item:
-            result_list.append(item)
-    print(tabulate(result_list, floatfmt='grid'))
-
-searching(separated_file_into_list_by_dic())
+print('NOSIR' == 'nosir')
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tabulate import tabulate
 breaking_the_loop = True
 
-
+#add
 def writing_file(name, phone, email):
     """This function for adding new contact name phone and email"""
     f = open('data.txt', 'a')
@@ -30,31 +30,35 @@ def separated_file_into_list_by_dic():
     free_list.insert(0, ['Name', 'Phone number', 'E-mail'])
 
     return free_list
-
+#contacts
 def show_contact_in_table():
     result = separated_file_into_list_by_dic()
-    print(tabulate(result, tablefmt='grid'))
+    print(tabulate(result, tablefmt='psql', headers='firstrow', showindex='always'))
 
-def searching(key_word):
+def searching(key_word): 
+    result_list = []
     for item in separated_file_into_list_by_dic():
-        result_list = []
-        if key_word in item:
+        if key_word.lower() in ''.join(item).lower():
             result_list.append(item)
-    print(tabulate(result_list, floatfmt='grid'))
+    result_list.insert(0, ['Name', 'Phone Number', 'E-mail'])
+    print(tabulate(result_list, headers='firstrow', floatfmt='grid', showindex='always'))
 
         
 
 
 while breaking_the_loop:
     command = input('Type help to see commands list: ')
-
-    if command.lower() == 'add':
+    #completed
+    if command.lower() == 'add': 
         name = input('Enter name: ')
         phone = input('Enter phone: ')
         email = input('Enter email: ')
         writing_file(name, phone, email)
-    elif command.lower() == 'search':
-        search_input = input()
+    #completed
+    elif command.lower() == 'search':  
+        search_input = input('Input key word to search: ')
+        searching(search_input)
+    #completed
     elif command.lower() == 'exit':
         print('')
         print(f'-----------')
@@ -62,6 +66,7 @@ while breaking_the_loop:
         print(f'-----------')
         print('')
         breaking_the_loop = False
+    #completed
     elif command.lower() == 'help':
         print(
         '''
@@ -72,8 +77,17 @@ while breaking_the_loop:
         "search" for seacrching.       
         '''
         )
+    #completed
     elif command.lower() == 'contacts':
         show_contact_in_table()
+    # elif command.lower() == 'delete':
+    #     index = int(input('Input the index for del: '))
+    #     index += 1
+    #     deleted_list_index = []
+    #     deleted_list_index.append(index)
+    #     print(deleted_list_index)
+    
+    #completed Incorrect input part
     else:
         print("""
 
